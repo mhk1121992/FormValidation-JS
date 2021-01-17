575f5f68b284547beaf88d4a230bb75363305985
Mahdi Hasan Khan <mhk1121992@gmail.com>
2021-01-17 21:31:44 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,85 @@
+const form = document.getElementById('form');
+const password1El = document.getElementById('password1');
+const password2El = document.getElementById('password2');
+const messageContainer = document.querySelector('.message-container');
+const message = document.getElementById('message');
+
+let isValid = false;
+let passwordsMatch = false;
+
+function validateForm()
+{
+  // using constraint API
+  isValid = form.checkValidity();
+  console.log(isValid);
+
+  // style main message for error
+  if(!isValid)
+  {
+    message.textContent = 'Please fill out all fields.';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
+    return;
+  }
+
+  // check to see if passwords match
+  if(password1El.value === password2El.value)
+  {
+    passwordsMatch = true;
+    password1El.style.borderColor = 'green';
+    password2El.style.borderColor = 'green';
+  }
+  else{
+    passwordsMatch = false;
+    message.textContent = 'Make sure passwords match';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
+    password1El.style.borderColor = 'red';
+    password2El.style.borderColor = 'red';
+    return;
+    
+  }
+
+  // if form is valid and passwords match
+  if(isValid && passwordsMatch)
+  {
+    message.textContent = 'Successfully Reigstered!';
+    message.style.color = 'green';
+    messageContainer.style.borderColor = 'green';
+  }
+  
+
+
+}
+
+function storeFormData()
+{
+  const user = {
+    name: form.name.value,
+    phone: form.phone.value,
+    email: form.email.value,
+    website: form.website.value,
+    password: form.password.value
+  };
+
+  // Do something using user data
+  console.log(user);
+}
+
+
+function processFormData(e)
+{
+  e.preventDefault();  
+  
+  //validate form
+  validateForm();
+
+  //submit data if it's valid
+
+  if(isValid && passwordsMatch)
+  {
+    storeFormData();
+  }
+}
+// event listener
+form.addEventListener('submit', processFormData);
